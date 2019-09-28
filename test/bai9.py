3ab3f0f8911c35bbf822cfb7f4cc94239d8f4327
GX18 <phambichan0505@gmail.com>
2019-09-28 20:45:11 +0700

    Fix1

--- test/bai9.py
+++ test/bai9.py
@@ -1,16 +1,18 @@
 a = int(input("Cho một số nào"))
 b = int(input("Cho thêm số nữa!"))
 c = int(input("Và số cuối cùng!"))
-delta = b*b - 4*a*c
-if delta < 0:
-    print("Phương trình vô nghiệm")
-elif delta == 0:
-    x = -b /(2*a)
-    print(x)
-elif delta > 0:
-    x1 = (-b + delta**(1/2))/(2*a) 
-    x2 = (-b - delta**(1/2))/(2*a)
-    print("Phương trình có hai nghiệm phân biệt",x1, ",", x2)
+def quadro(a,b,c):
+    delta = b*b - 4*a*c
+    if delta < 0:
+        print("Phương trình vô nghiệm")
+    elif delta == 0:
+        x = -b /(2*a)
+        print(x)
+    elif delta > 0:
+        x1 = (-b + delta**(1/2))/(2*a) 
+        x2 = (-b - delta**(1/2))/(2*a)
+        print("Phương trình có hai nghiệm phân biệt",x1, ",", x2)
+quadro(a,b,c)
 
 
 
